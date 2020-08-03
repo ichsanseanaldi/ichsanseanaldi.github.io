c633d557998c337c76ab683d2ded663e2906bd81
Ichsan Seanaldi <ichsanseanaldi10@gmail.com>
2020-08-03 17:16:51 +0700

    fixing some stuff

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const link = document.querySelectorAll('.link');
+// const link = document.querySelectorAll('.link');
 
-for (let i = 0; i < link.length; i++) {
-	link[i].addEventListener('click', function (e) {
-		let active = document.querySelectorAll('.active');
+// for (let i = 0; i < link.length; i++) {
+// 	link[i].addEventListener('click', function (e) {
+// 		let active = document.querySelectorAll('.active');
 
-		if (active.length > 0) {
-			document.querySelector('.active').classList.remove('active');
-			e.target.classList.toggle('active');
-		} else {
-			e.target.classList.add('active');
-		}
-	});
-}
+// 		if (active.length > 0) {
+// 			document.querySelector('.active').classList.remove('active');
+// 			e.target.classList.toggle('active');
+// 		} else {
+// 			e.target.classList.add('active');
+// 		}
+// 	});
+// }
 
 const fill = document.querySelectorAll('.fill');
 
