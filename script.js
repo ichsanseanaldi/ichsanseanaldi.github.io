608357cfb9a728c3ebad10c2a1f4322beb61241d
Ichsan Seanaldi <ichsanseanaldi10@gmail.com>
2020-08-19 15:27:01 +0700

    reducing javascript numbers

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const link = document.querySelectorAll('.link');
-
-// for (let i = 0; i < link.length; i++) {
-// 	link[i].addEventListener('click', function (e) {
-// 		let active = document.querySelectorAll('.active');
-
-// 		if (active.length > 0) {
-// 			document.querySelector('.active').classList.remove('active');
-// 			e.target.classList.toggle('active');
-// 		} else {
-// 			e.target.classList.add('active');
-// 		}
-// 	});
-// }
-
 const fill = document.querySelectorAll('.fill');
 
 for (let i = 0; i < fill.length; i++) {
@@ -44,38 +29,16 @@ window.onscroll = () => {
 		btn.style.opacity = '0';
 		btn.style.visibility = 'hidden';
 	}
-
-	// Script below is not effective, but it's the best i can do, Dont repeat urself!!
-
-	// if (document.documentElement.scrollTop >= 0 & document.documentElement.scrollTop < 500) {
-	// 	links[0].classList.add('active');
-	// } else {
-	// 	links[0].classList.remove('active');
-	// }
-	// if (document.documentElement.scrollTop >= 550 & document.documentElement.scrollTop < 1000) {
-	// 	links[1].classList.add('active');
-	// } else {
-	// 	links[1].classList.remove('active');
-	// }
-	// if (document.documentElement.scrollTop >= 1100 & document.documentElement.scrollTop < 2640) {
-	// 	links[2].classList.add('active');
-	// } else {
-	// 	links[2].classList.remove('active');
-	// }
-
-
 }
-
-//script below are from stack overflow
 
 const sections = document.querySelectorAll('section');
 
 function changeLinkState() {
 	let index = sections.length;
 	while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
 	links.forEach((link) => link.classList.remove('active'));
 	links[index].classList.add('active');
+
 }
 
 changeLinkState();
