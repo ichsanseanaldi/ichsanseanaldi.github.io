d017df46f2dac45ec84b9d9013b16b41d3ef4f91
Ichsan Seanaldi <ichsanseanaldi10@gmail.com>
2020-09-03 16:47:36 +0700

    changing button to scroll

--- script.js
+++ script.js
@@ -4,38 +4,34 @@ const s = document.querySelectorAll('section');
 const n = document.querySelectorAll('.number');
 const g = document.querySelector('.scroll');
 
+setTimeout(() => {
+	n.forEach(el => {
+		let f = el.dataset.number;
+		let b = 0;
+		let ct = setInterval(() => {
+			if (b <= f) {
+				el.innerText = `${b}%`;
+				b++;
+			} else {
+				clearInterval(ct);
+			}
+		}, 1000 / 60, 'linear');
+	})
 
-const bounding = s[1].getBoundingClientRect();
+	z.forEach(e => {
+		let v = e.dataset.width;
+		let a = 0;
+		let c = setInterval(() => {
+			if (a <= v) {
+				e.style.width = `${a}%`;
+				a++;
+			} else {
+				clearInterval(c);
+			}
+		}, 1000 / 60, 'linear');
+	})
+}, 1000);
 
-if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-	alert('sup');
-}
-
-n.forEach(el => {
-	let f = el.dataset.number;
-	let b = 0;
-	let ct = setInterval(() => {
-		if (b <= f) {
-			el.innerText = `${b}%`;
-			b++;
-		} else {
-			clearInterval(ct);
-		}
-	}, 1000 / 60);
-})
-
-z.forEach(e => {
-	let v = e.dataset.width;
-	let a = 0;
-	let c = setInterval(() => {
-		if (a <= v) {
-			e.style.width = `${a}%`;
-			a++;
-		} else {
-			clearInterval(c);
-		}
-	}, 1000 / 60);
-})
 
 window.addEventListener('scroll', () => {
 	(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) ? g.classList.add('show'): g.classList.remove('show');
