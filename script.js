256db8b80f9ef5b8d6e83b611eae67d795764e8f
Ichsan Seanaldi <ichsanseanaldi10@gmail.com>
2020-09-07 09:28:43 +0700

    bug fix

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const s = document.querySelectorAll('section');
 const n = document.querySelectorAll('.number');
 const g = document.querySelector('.scroll');
 
+g.addEventListener('click', () => {
+	if (document.scrollingElement.scrollTop === 0) return;
+
+	const totalScrollDistance = document.scrollingElement.scrollTop;
+
+	let scrollY = totalScrollDistance;
+	let oldTimestamp = null;
+
+	function step(newTimestamp) {
+		if (oldTimestamp !== null) {
+			scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / 700;
+			if (scrollY <= 0) {
+				return document.scrollingElement.scrollTop = 0;
+			}
+			document.scrollingElement.scrollTop = scrollY;
+		}
+		oldTimestamp = newTimestamp;
+		window.requestAnimationFrame(step);
+	}
+	window.requestAnimationFrame(step);
+});
+
 setTimeout(() => {
 	n.forEach(el => {
 		let f = el.dataset.number;
